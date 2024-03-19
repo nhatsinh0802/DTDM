92ae5b777776e699c2fe9c644228bcd46ebe23f5
nhatsinh0802 <106651545+nhatsinh0802@users.noreply.github.com>
2024-03-19 14:28:14 +0700

    Add files via upload

---
+++ Bai1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	
+	int i;
+
+	printf("Cac so nguyen co 2 chu so va la boi cua 7:\n");
+
+	for (i = 7; i <= 99; i += 7) {
+		printf("%d ", i);
+	}
+	
+	getchar();
+	return 0;
+}
---
+++ Bai2.cpp
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+#include <math.h>
+// Hàm kiểm tra số chính phương
+int isPerfectSquare(int num) {
+	int i;
+	for (i = 1; i * i <= num; i++) {
+		if (i * i == num)
+			return 1;
+	}
+	return 0;
+}
+
+// Hàm đếm và in ra các số chính phương nhỏ hơn n
+void printPerfectSquares(int n) {
+	int count = 0;
+
+	printf("Các số chính phương nhỏ hơn %d:\n", n);
+
+	for (int i = 1; i < n; i++) {
+		if (isPerfectSquare(i)) {
+			printf("%d ", i);
+			count++;
+		}
+	}
+
+	printf("\nTổng số chính phương: %d\n", count);
+}
+
+int main() {
+	int n;
+
+	printf("Nhập n: ");
+	scanf_s("%d", &n);
+
+	printPerfectSquares(n);
+
+	// Đợi người dùng nhấn phím bất kỳ trước khi kết thúc chương trình
+	getchar();
+	return 0;
+}
